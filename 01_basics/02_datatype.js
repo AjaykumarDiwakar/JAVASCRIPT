fe3d9ad39ebe4bbfbf6055bc83f3db5785c834d6
Ajay Kumar <144464750+AjaykumarDiwakar@users.noreply.github.com>
2024-08-26 18:16:13 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,17 @@
+// datatypes in js
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Object
+
+let a="Ajay"
+let b=2322
+let c=122222222211222222222222222222222222222222222222222222222222222222222
+let d=true
+let e
+let f=null
+console.table([typeof a,typeof b,typeof c,typeof d,typeof e,typeof f])
