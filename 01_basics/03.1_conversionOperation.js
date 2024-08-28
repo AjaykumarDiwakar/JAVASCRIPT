67b4e340c41890e7b1b38e0afa97b0e214538f1e
Ajay Kumar <144464750+AjaykumarDiwakar@users.noreply.github.com>
2024-08-28 15:14:08 +0000

    280824:843:P

---
+++ 01_basics/03.1_conversionOperation.js
@@ -0,0 +1,30 @@
+let value=23
+let negValue=-value
+console.log(negValue);
+console.log("2+ 3 = "+2+3);
+console.log("2-3 = "+2-3);
+console.log("2*3= "+2*3);
+console.log("3/2= "+3/2);
+console.log("floor divison in 3/2= "+Math.floor(3/2));
+console.log("3**2= "+3**2);
+console.log("3%2 = "+3%2);
+
+
+// String concatenation
+
+let str="Ajay"
+let str1="Kumar"
+
+let name1=str+str1
+console.log(name1);
+
+
+// some game with string and number datatype combined
+
+console.log("1"+2)
+console.log(2+"1");
+console.log("1"+2+3);
+console.log(1+"2"+3);
+console.log(1+2+"3");
+
+
