9b6623dca31370a25070a5657558c50f1ebcabf4
Ajay Kumar <144464750+AjaykumarDiwakar@users.noreply.github.com>
2024-09-04 15:35:19 +0000

    04.09.2024:9:02:P

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,41 @@
+// Immediately Invoked Function Expression
+
+(function look() {
+    console.log("Hi Aj,This is look function");  
+})();
+
+// have a look at the syntax
+/*
+(->outer parenthesis in which method is declared
+
+Method body
+
+)()->Here the Parenthesis for invokation as we used ealier like func()
+
+it is reccommended to use the semicolon after the iife ,as it semicolon refers to the end of 
+the function  execution otherwise if we write any iife after it(current iife) we will get error.
+*/ 
+// (
+// function name() {
+//     console.log("Hi ,Ajay");
+    
+// }
+
+// )()
+
+(()=>{
+    console.log("We can use arrow function within a iife...");
+})();
+
+//we are allowed to pass the parameter in the iife 
+
+((name)=>{
+    console.log("Hello ,",name);
+})("Ajay");
+
+(function(name){
+    console.log("Hello ,",name);
+    
+});
+
+
