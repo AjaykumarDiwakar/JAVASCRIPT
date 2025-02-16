0e08b321a36f0b72f01c005c99d76ffac75a089e
AjayKumarDiwakar <kumarajay7160@gmail.com>
2025-02-16 15:50:55 +0530

    print half of the array

---
+++ Restart/Array/First.js
@@ -0,0 +1,10 @@
+const arr=["A","B","C","D","E","F","G"];
+//!Print first half of the elements of the array.
+for(let i=0;i<Math.floor(arr.length/2);i++){
+    console.log(arr[i]);
+}
+
+for(let i=0;i<arr.length/2;i++){
+    console.log(arr[i]);
+}
+
